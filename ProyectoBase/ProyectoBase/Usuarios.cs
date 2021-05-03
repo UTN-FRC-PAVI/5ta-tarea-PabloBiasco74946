029467fbcca9ebc65bf6a90bb8634915c111abb1
PabloBiasco74946 <81331021+PabloBiasco74946@users.noreply.github.com>
2021-05-03 20:37:16 -0300

    PAV 1 Clase 5
    Clase 5

---
+++ ProyectoBase/ProyectoBase/Usuarios.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoBase
+{
+    public partial class Usuarios : Form
+    {
+        public Usuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Usuarios_Load(object sender, EventArgs e)
+        {
+            CargarGrilla();
+        }
+
+        private void CargarGrilla()
+        {
+
+
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+
+            try
+            {
+                bool resultado = false;
+                SqlCommand cmd = new SqlCommand();
+                string consulta = "SELECT * FROM usuarios";
+
+                cmd.Parameters.Clear();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+
+                DataTable tabla = new DataTable();
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabla);
+                grillaUsuarios.DataSource = tabla;
+                if (tabla.Rows.Count == 1)
+                {
+                    resultado = true;
+                }
+
+                else
+                {
+                    resultado = false;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+        }
+        private void LimpiarCampos()
+        {
+            txtNombreDeUsuario.Text = "";
+            txtPassword.Text = "";
+            txtRepetirPassword.Text = "";
+        }
+
+        private void btnAltaUsuario_Click(object sender, EventArgs e)
+        {
+            if(txtNombreDeUsuario.Text.Equals(""))
+            {
+                MessageBox.Show("Ingrese nombre de usuario!");
+            }
+            else
+            {
+                if(txtPassword.Text.Equals(txtRepetirPassword.Text) == true)
+                {
+                    try
+                    {
+                        bool resultado = InsertarUsuario(txtNombreDeUsuario.Text, txtPassword.Text);
+                        if (resultado)
+                        {
+                            MessageBox.Show("Usuario dado de alta exitosamente!");
+                            LimpiarCampos();
+                            CargarGrilla();
+                            txtNombreDeUsuario.Focus();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error al dar de alta al usuario");
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                        MessageBox.Show("Error al insertar nuevo usuario...");
+                        txtNombreDeUsuario.Focus();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Los passwords no coinciden");
+                }
+            }
+        }
+        private bool InsertarUsuario(string nombreDeUsuario, string password)
+        {
+            string cadenaConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaConexion);
+            bool resultado = false;
+            
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                string consulta = "INSERT INTO usuarios (NombreDeUsuario, Password) VALUES(@nombreUsu, @pass)";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@nombreUsu", nombreDeUsuario);
+                cmd.Parameters.AddWithValue("@pass", password);
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+                cmd.ExecuteNonQuery();
+                resultado = true;
+
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+            
+
+
+
+
+            return resultado;
+        }
+    
+    }
+   
+}
